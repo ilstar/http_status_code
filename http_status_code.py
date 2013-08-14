b3ed74429deee0867dabc894424b338910726633
Fred Liang(ilstar) <fred.liang.q@gmail.com>
2013-08-14 14:40:30 +0800

    Not case sensitive, open the url directly by select a certain result

---
+++ http_status_code.py
@@ -0,0 +1,29 @@
+'''
+HTTP Status Code v0.1
+
+Github: https://github.com/ilstar/http_status_code
+Author: Fred Liang
+'''
+
+import csv
+
+from feedback import Feedback
+
+query = '{query}'
+query = query.lower()
+baseurl = 'http://en.wikipedia.org/wiki/List_of_HTTP_status_codes#'
+
+fb = Feedback()
+
+with open('status_code.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        code, desc = row
+        lower_desc = desc.lower()
+
+        if code.find(query) != -1:
+            fb.add_item(desc, code, arg=baseurl + code)
+        elif lower_desc.find(query) != -1:
+            fb.add_item(code, desc, arg=baseurl + code)
+
+print fb
